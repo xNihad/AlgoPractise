78a84704705130f3edf0a8e64103141732290981
xNihad <nxankishiyev@std.beu.edu.az>
2024-07-21 16:32:37 +0400

    Task 11 is solved

--- Algo-11/script.js
+++ Algo-11/script.js
@@ -1,14 +1,14 @@
-let arr = [0,1,2,3,4,5,67]
-
-function SumOfNumbers(a){
-    let max=1
-    for(let i=0; i<arr.length;i++){
-        if(a[i]>max){
-            max=a[i]   
+// 11.Bir funksiya yazın.Bu funksiya ardıcıl ədədlərdən ibarət bir arrayi 
+// parametr şəklində qəbul etsin və ardıcıllıqdakı çatışmayan ədədi return etsin.
+// Məsələn: FindNumber([1,2,3,4,5,6,8,9,10]) => OutPut 7
+let gather = (a)=>{
+    for(let i = 0; i <=a.length; i++){
+        if((a[i]+a[i+2])/2 != a[i+1]){
+            return a[i+1]
         }
     }
-    return max
     
 }
-let res = SumOfNumbers(arr)
+let arr = [1,2,3,4,5,6,8,9,10]
+let res = gather(arr)
 console.log(res);
