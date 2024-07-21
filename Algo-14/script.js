ca09567b3f98580507c8c6ff0b80eb6806b98c4e
xNihad <nxankishiyev@std.beu.edu.az>
2024-07-21 20:41:24 +0400

    Task 14 is solved

--- Algo-14/script.js
+++ Algo-14/script.js
@@ -1,14 +1,24 @@
-let arr = [0,1,2,3,4,5,67]
+// 14.Bir funksiya yazın iki parametr qəbul etsin. Birinci parametr rəqəmlərdən ibarət array, 
+// ikinici parametr isə rəqəm olsun. Arraydəki bütün  rəqəmlər daxil edilən ikinci 
+// parametrin bölənləridirsə, funksiya true return etsin, yox bir dənəsi belə böləni 
+// deyilsə false return etsin.
 
-function SumOfNumbers(a){
-    let max=1
+let confuse = (arr, num)=>{
+    let count=0;
     for(let i=0; i<arr.length;i++){
-        if(a[i]>max){
-            max=a[i]   
+        if(num%arr[i]==0) {
+            count++;
+        }else {
+            continue;
         }
     }
-    return max
-    
+    if(count==arr.length) {
+        return true;
+    }else{
+       return false;
+    } 
 }
-let res = SumOfNumbers(arr)
+let arrayx = [1,2,3,4,5,6,7,8,9,10]
+let numberx = 5040
+let res = confuse(arrayx, numberx)
 console.log(res);
